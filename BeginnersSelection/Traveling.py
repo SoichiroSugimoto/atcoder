d42b9d26c6e97bcf068003c08c05fe9c9804bbdb
杉本創一朗 <fcsoichi1998@gmail.com>
2023-03-11 01:49:48 +0900

    Traveling

--- BeginnersSelection/Traveling.py
+++ BeginnersSelection/Traveling.py
@@ -1,7 +1,19 @@
 N = int(input())
-t = [list(map(int, input().split()))  for i in range(N)]
+p = [list(map(int, input().split()))  for i in range(N)]
+p.insert(0, [0, 0, 0])
 
-x = 0
-_x = 0
-y = 0
-_y = 0
+for n in range(N):
+  t =  p[n + 1][0] - p[n][0]
+  x =  p[n + 1][1] - p[n][1]
+  y =  p[n + 1][2] - p[n][2]
+  flg = 0
+  for a in range(t):
+    b = t - a
+    if (abs(a - b) == abs(x + y)):
+      flg = 1
+      break
+  if (flg == 0):
+    print ("No")
+    exit()
+
+print ("Yes")
