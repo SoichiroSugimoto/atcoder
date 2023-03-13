a6b017fcf3501e810e9cf19c2139e851f5e711f2
杉本創一朗 <fcsoichi1998@gmail.com>
2023-03-14 03:00:12 +0900

    tmp

--- ABC292/FourVariables.py
+++ ABC292/FourVariables.py
@@ -1,17 +1,32 @@
 n = int(input())
 cnt = 0
+mid = 0
+tmp = 0
 
 for ab in range(n):
   cd = n - ab
+  if (ab == cd):
+    tmp = cnt
   if (ab > 0 and cd > 0):
     ab_cnt = 0
     cd_cnt = 0
-    for a in range(ab + 1):
-      if (a > 0 and ab / a % 1 == 0):
+    a = 0
+    while (a * a <= ab):
+      if (a > 0 and ab % a == 0):
         ab_cnt += 1
-    for c in range(cd + 1):
-      if (c > 0 and cd / c % 1 == 0):
+        if (ab != a * a):
+          ab_cnt += 1
+      a += 1
+    c = 0
+    while (c * c <= cd):
+      if (c > 0 and cd % c == 0):
         cd_cnt += 1
+        if (cd != c * c):
+          cd_cnt += 1
+      c += 1
     cnt += (ab_cnt * cd_cnt)
+  if (ab == cd):
+    mid = cnt - tmp
+    break
 
-print (cnt)
+print (tmp * 2 + mid)
