ef2b72dbfea4401e75cb8fdd02df55b91272795d
杉本創一朗 <fcsoichi1998@gmail.com>
2023-03-25 17:46:37 +0900

    MakeTakahashiHapp

--- ABC293/MakeTakahashiHappy.py
+++ ABC293/MakeTakahashiHappy.py
@@ -1,4 +1,24 @@
 hw = input().split()
-a = [list(map(int, input().split()))  for i in range(int(hw[0]))]
+grid = [list(map(int, input().split()))  for i in range(int(hw[0]))]
 
-# 全探索アルゴリズム
+ans = 0
+height = int(hw[0])
+width = int(hw[1])
+
+def func(i, j, via):
+  global ans
+  while (i + j - 2 < len(via)):
+    via.pop(len(via) - 1)
+  if (i > height or j > width):
+    return False
+  via.append(grid[i-1][j-1])
+  if (i == height and j == width and len(set(via)) == height + width - 1):
+    ans += 1
+  if (func(i+1, j, via)):
+    return True
+  if (func(i, j+1, via)):
+    return True
+  return False
+
+func(1, 1, [])
+print(ans)
