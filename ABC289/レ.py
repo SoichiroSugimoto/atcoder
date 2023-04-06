a6251ee7aff5fa4bfe1d5deeed8bf0957232f9b7
杉本創一朗 <fcsoichi1998@gmail.com>
2023-04-07 01:40:07 +0900

    tmp

--- ABC289/レ.py
+++ ABC289/レ.py
@@ -7,31 +7,8 @@ print(NM)
 print(a)
 print(p)
 
-# for r in a:
-#   tmp = p[r - 1]
-#   p[r - 1] = p[r]
-#   p[r] = tmp
-#   print(r, p)
 
-flg = 0
-ans = []
-tmp = a[0]
+for num in NM[0]:
 
-for i in range(len(a)):
-  r = a[i]
-  rsv = []
-  print("----- ", r, tmp)
-  while (r == tmp + 1 and i < len(a)):
-    r = a[i]
-    print(r, tmp, tmp + 1)
-    rsv.append(p[r - 1])
-    rsv.append(p[r])
-    i += 1
-    print(rsv)
-    tmp = r
-  tmp = r
-  rsv.reverse()
-  print(rsv)
-  ans.append(rsv)
 
 print(ans)
