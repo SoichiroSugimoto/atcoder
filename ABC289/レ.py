165d9647a1b6af7e3bc8c417f8a96c3c8fe26c4d
杉本創一朗 <fcsoichi1998@gmail.com>
2023-04-08 14:23:10 +0900

    レ

--- ABC289/レ.py
+++ ABC289/レ.py
@@ -1,14 +1,23 @@
 NM = list(map(int, input().split()))
 a = list(map(int, input().split()))
 
-p = [x for x in range(NM[0] + 1) if x > 0] 
+ans = []
 
-print(NM)
-print(a)
-print(p)
+i = 0
+while (i < NM[0]):
+  b = []
+  c = a.count(i + 1)
+  if (c == 0):
+    ans.append([i + 1])
+  while (i < NM[0] and c == 1):
+    b.append(i + 1)
+    i += 1
+    c = a.count(i + 1)
+  if (len(b) != 0):
+    b.append(b[-1] + 1)
+    b.reverse()
+    ans.append(b)
+  i += 1
 
-
-for num in NM[0]:
-
-
-print(ans)
+ans = map(str, sum(ans, []))
+print(' '.join(ans))
