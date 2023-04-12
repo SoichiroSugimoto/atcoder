24e2f756681d4073cd21711059cc90372ddd321c
杉本創一朗 <fcsoichi1998@gmail.com>
2023-04-13 01:22:41 +0900

    tmp

--- ABC287/PathGraph.py
+++ ABC287/PathGraph.py
@@ -1,6 +1,49 @@
 NM = list(map(int, input().split()))
 V = [list(map(int, input().split()))  for i in range(NM[1])]
 
+
+class UnionFind:
+  def __init__(self, n):
+    self.parent = list(range(n))
+    self.rank = [0] * n
+
+  def find(self, x):
+    if self.parent[x] == x:
+      return x
+    else:
+      self.parent[x] = self.find(self.parent[x])
+      return self.parent[x]
+
+  def union(self, x, y):
+    root_x = self.find(x)
+    root_y = self.find(y)
+
+    if root_x == root_y:
+      return
+
+    if self.rank[root_x] > self.rank[root_y]:
+      self.parent[root_y] = root_x
+    else:
+      self.parent[root_x] = root_y
+      if self.rank[root_x] == self.rank[root_y]:
+        self.rank[root_y] += 1
+
+def is_connected(n, edges):
+  uf = UnionFind(n)
+  for edge in edges:
+    uf.union(edge[0], edge[1])
+
+  root = uf.find(0)
+  for i in range(1, n):
+    if uf.find(i) != root:
+      return 0
+  return 1
+
+print(V)
+conn = is_connected(NM[0], V)
+
+
+
 # print(V)
 p =  [0 for i in range(NM[0])]
 
@@ -10,7 +53,7 @@ for v in V:
 
 # print(NM[0], NM[1], p)
 
-if (p.count(1) == 2 and p.count(2) == NM[0] - 2):
+if (conn == 0 and NM[0] - 1 == NM[1] and p.count(1) == 2 and p.count(2) == NM[0] - 2):
   print("Yes")
 else:
   print("No")
--- Algorism/union_find.py
+++ Algorism/union_find.py
@@ -1,4 +1,5 @@
 def find(parent, x):
+    print(parent, x)
     if parent[x] == x:
         return x
     parent[x] = find(parent, parent[x])
@@ -7,17 +8,23 @@ def find(parent, x):
 def union(parent, x, y):
     x_root = find(parent, x)
     y_root = find(parent, y)
+    print("root", x_root, y_root)
     parent[x_root] = y_root
 
 def same(parent, x, y):
     return find(parent, x) == find(parent, y)
 
 # 使用例
-n = 5
+n = 10
 parent = list(range(n))
 
 union(parent, 1, 2)
-union(parent, 3, 4)
+print("------------------------------")
+union(parent, 5, 6)
+print("------------------------------")
+union(parent, 1, 5)
+print(parent)
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
-print(same(parent, 1, 2))  # True: 1と2は同じグループに属しています
-print(same(parent, 1, 3))  # False: 1と3は異なるグループに属しています
+# print(same(parent, 1, 2))  # True: 1と2は同じグループに属しています
+print(same(parent, 2, 6))  # False: 1と3は異なるグループに属しています
