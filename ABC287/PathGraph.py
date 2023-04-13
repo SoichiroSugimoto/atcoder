718f85aed57317c242856bb055731e2e299a2e6d
杉本創一朗 <fcsoichi1998@gmail.com>
2023-04-14 02:18:07 +0900

    tmp

--- ABC287/PathGraph.py
+++ ABC287/PathGraph.py
@@ -2,58 +2,43 @@ NM = list(map(int, input().split()))
 V = [list(map(int, input().split()))  for i in range(NM[1])]
 
 
-class UnionFind:
-  def __init__(self, n):
-    self.parent = list(range(n))
-    self.rank = [0] * n
+def find(parent, x):
+    print(parent, x)
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent, parent[x])
+    return parent[x]
 
-  def find(self, x):
-    if self.parent[x] == x:
-      return x
-    else:
-      self.parent[x] = self.find(self.parent[x])
-      return self.parent[x]
+def union(parent, x, y):
+    x_root = find(parent, x)
+    y_root = find(parent, y)
+    print("root", x_root, y_root)
+    parent[x_root] = y_root
 
-  def union(self, x, y):
-    root_x = self.find(x)
-    root_y = self.find(y)
+def same(parent, x, y):
+    return find(parent, x) == find(parent, y)
 
-    if root_x == root_y:
-      return
-
-    if self.rank[root_x] > self.rank[root_y]:
-      self.parent[root_y] = root_x
-    else:
-      self.parent[root_x] = root_y
-      if self.rank[root_x] == self.rank[root_y]:
-        self.rank[root_y] += 1
-
-def is_connected(n, edges):
-  uf = UnionFind(n)
-  for edge in edges:
-    uf.union(edge[0], edge[1])
-
-  root = uf.find(0)
-  for i in range(1, n):
-    if uf.find(i) != root:
-      return 0
-  return 1
-
-print(V)
-conn = is_connected(NM[0], V)
 
 
 
 # print(V)
-p =  [0 for i in range(NM[0])]
+parent =  [i for i in range(NM[0])]
+
+print(parent)
 
 for v in V:
-  p[v[0] - 1] += 1
-  p[v[1] - 1] += 1
+  union(parent, v[0] - 1, v[1] - 1)
 
-# print(NM[0], NM[1], p)
+print(parent)
 
-if (conn == 0 and NM[0] - 1 == NM[1] and p.count(1) == 2 and p.count(2) == NM[0] - 2):
-  print("Yes")
-else:
-  print("No")
+
+# for v in V:
+#   p[v[0] - 1] += 1
+#   p[v[1] - 1] += 1
+
+# # print(NM[0], NM[1], p)
+
+# if (conn == 0 and NM[0] - 1 == NM[1] and p.count(1) == 2 and p.count(2) == NM[0] - 2):
+#   print("Yes")
+# else:
+#   print("No")
