5ca9ed4a20e43d1cc43c4185d73b52565c43859e
杉本創一朗 <fcsoichi1998@gmail.com>
2023-04-15 14:41:35 +0900

    tmp

--- ABC287/PathGraph.py
+++ ABC287/PathGraph.py
@@ -1,44 +1,33 @@
-NM = list(map(int, input().split()))
-V = [list(map(int, input().split()))  for i in range(NM[1])]
-
+N, M = map(int, input().split())
+V = [list(map(int, input().split())) for i in range(M)]
 
 def find(parent, x):
-    print(parent, x)
-    if parent[x] == x:
-        return x
-    parent[x] = find(parent, parent[x])
-    return parent[x]
+  if (parent[x] == x):
+    return x
+  return find(parent, parent[x])
 
 def union(parent, x, y):
-    x_root = find(parent, x)
-    y_root = find(parent, y)
-    print("root", x_root, y_root)
-    parent[x_root] = y_root
+  x_root = find(parent, x)
+  y_root = find(parent, y)
+  parent[x_root] = y_root
 
 def same(parent, x, y):
-    return find(parent, x) == find(parent, y)
+  return find(parent, x) == find(parent, y)
 
-
-
-
-# print(V)
-parent =  [i for i in range(NM[0])]
-
-print(parent)
+parent = [i for i in range(N)]
+p = [0 for i in range(N)]
 
 for v in V:
   union(parent, v[0] - 1, v[1] - 1)
+  p[v[0] - 1] += 1
+  p[v[1] - 1] += 1
 
-print(parent)
+conn = 0
+for i in range(N):
+  if (same(parent, parent[0], parent[i])):
+    conn += 1
 
-
-# for v in V:
-#   p[v[0] - 1] += 1
-#   p[v[1] - 1] += 1
-
-# # print(NM[0], NM[1], p)
-
-# if (conn == 0 and NM[0] - 1 == NM[1] and p.count(1) == 2 and p.count(2) == NM[0] - 2):
-#   print("Yes")
-# else:
-#   print("No")
+if (conn == N and p.count(1) == 2 and p.count(2) == N - 2):
+  print("Yes")
+else:
+  print("No")
